703fb98a7d0eed8c267b1efee1655bea79aeb413
Rishisup3ryo <117019636+DISAPPOINTED-GUY@users.noreply.github.com>
2024-07-25 22:05:11 +0530

    Create keys.py

---
+++ api/keys.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+@app.route('/api/keys')
+def get_keys():
+    keys = {
+        "confidential": os.getenv("CONFIDENTIAL"),
+        "tvscientist": os.getenv("UTTV"),
+        "secret_key": os.getenv("SECRET_KEY"),
+        "repl_owner": os.getenv("REPL_OWNER")
+    }
+    return jsonify(keys)
+
+if __name__ == '__main__':
+    app.run()
+  
